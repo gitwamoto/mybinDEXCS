bdf8ad91141e831dab575f28e2ab24b78a85eede
gitwamoto <iwamoto.yukiharu.mc@ehime-u.ac.jp>
2026-03-17 23:58:18 +0900

    Update folderTime.py

--- binDEXCS（解析フォルダを端末で開いてから）/utilities/folderTime.py
+++ binDEXCS（解析フォルダを端末で開いてから）/utilities/folderTime.py
@@ -2,10 +2,9 @@
 # -*- coding: utf-8 -*-
 # folderTime.py
 # by Yukiharu Iwamoto
-# 2025/8/5 2:44:02 PM
+# 2026/3/17 11:56:53 PM
 
 import os
-import glob
 
 def firstTime(path = os.curdir):
     t = timesFolders(path)
@@ -16,14 +15,14 @@ def latestTime(path = os.curdir):
     return None if len(t) == 0 else t[-1]
 
 def timesFolders(path = os.curdir):
-    t = []
-    for d in glob.iglob(os.path.join(path, '*' + os.sep)):
-        d = os.path.basename(os.path.dirname(d))
-        try:
-            t.append([d, float(d)])
-        except:
-            pass
-    return [i[0] for i in sorted(t, key = lambda x: x[1])] 
+    times = []
+    for i in os.listdir(path):
+        if os.path.isdir(os.path.join(path, i)):
+            try:
+                times.append([i, float(i)])
+            except:
+                pass
+    return [i[0] for i in sorted(times, key = lambda x: x[1])]
 
 if __name__ == '__main__':
     print('firstTime = ' + firstTime())
