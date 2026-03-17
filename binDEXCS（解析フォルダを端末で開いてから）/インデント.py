8469a942327a523e0acff3484bafe748c3a5dd2a
gitwamoto <98292412+gitwamoto@users.noreply.github.com>
2026-03-17 22:34:26 +0900

    Update インデント.py

--- binDEXCS（解析フォルダを端末で開いてから）/インデント.py
+++ binDEXCS（解析フォルダを端末で開いてから）/インデント.py
@@ -2,32 +2,30 @@
 # -*- coding: utf-8 -*-
 # インデント.py
 # by Yukiharu Iwamoto
-# 2021/6/30 11:37:44 AM
+# 2026/3/17 10:34:10 PM
 
 # ---- オプションはない ----
 
+# DictParser2で書き直し済み
+
 import signal
-import subprocess
 import os
 import glob
 from utilities import misc
-from utilities import dictFormat
 from utilities import appendEntries
-from utilities.dictParse import DictParser
 from utilities import rmObjects
+from utilities import dictParse
 
 def indent(dir_name):
-    if not os.path.isdir(dir_name):
-        return
-    for f in glob.iglob(os.path.join(dir_name, '*')):
-        if os.path.isfile(f):
-            print('{}を処理中...'.format(f))
-            with open(f, 'r') as fp:
-                s_old = fp.read()
-            s = dictFormat.moveLineToBottom(DictParser(string = s_old)).toString()
-            if s != s_old:
-                with open(f, 'w') as fp:
-                    fp.write(s)
+    for i in glob.iglob(os.path.join(dir_name, '*')):
+        if os.path.isfile(i):
+            print(f'{i}を処理中...')
+            parser = dictParse.DictParser2(file_name = i)
+            string = dictParse.normalize(string = parser.file_string(pretty_print = True))[0]
+            if parser.string != string:
+#               os.rename(i, i + '_bak')
+                with open(i, 'w') as f:
+                    f.write(string)
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal.SIG_DFL) # Ctrl+Cで終了
@@ -36,14 +34,14 @@ if __name__ == '__main__':
     indent('0')
     indent('constant')
     indent('system')
-    for d in glob.iglob(os.path.join('0', '*' + os.sep)):
-        indent(d)
-    for d in glob.iglob(os.path.join('constant', '*' + os.sep)):
-        if os.path.isdir(os.path.join(d, 'polyMesh')):
-            indent(d)
-    for d in glob.iglob(os.path.join('system', '*' + os.sep)):
-        if os.path.isfile(os.path.join(d, 'fvSolution')):
-            indent(d)
+    for i in glob.iglob(os.path.join('0', '*' + os.sep)):
+        indent(i)
+    for i in glob.iglob(os.path.join('constant', '*' + os.sep)):
+        if os.path.isdir(os.path.join(i, 'polyMesh')):
+            indent(i)
+    for i in glob.iglob(os.path.join('system', '*' + os.sep)):
+        if os.path.isfile(os.path.join(i, 'fvSolution')):
+            indent(i)
 
     appendEntries.intoFvSolution()
     appendEntries.intoFvSchemes()
