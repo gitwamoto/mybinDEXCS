1d03ddfc35b03930ffeec8d1b7f3e73fbb5b01fa
gitwamoto <98292412+gitwamoto@users.noreply.github.com>
2026-03-17 22:21:02 +0900

    Update setFieldsを実行.py

--- binDEXCS（解析フォルダを端末で開いてから）/setFieldsを実行.py
+++ binDEXCS（解析フォルダを端末で開いてから）/setFieldsを実行.py
@@ -2,12 +2,14 @@
 # -*- coding: utf-8 -*-
 # setFieldsを実行.py
 # by Yukiharu Iwamoto
-# 2023/11/10 4:28:45 PM
+# 2026/3/17 10:20:51 PM
 
 # ---- オプション ----
 # なし -> インタラクティブモードで実行．オプションが1つでもあると非インタラクティブモードになる
 # -N -> 非インタラクティブモードで実行．system/setFieldsDictに設定値に関する指示を書き込んでいることが前提
 # -o -> 0秒のフォルダに.origで終わるファイルがある場合，それに対してsetFieldsを行う
+
+# DictParser2で書き直し済み
 
 import sys
 import signal
@@ -15,29 +17,10 @@ import subprocess
 import os
 import shutil
 from utilities import misc
-from utilities.dictParse import DictParser, DictParserList
 from utilities import rmObjects
+from utilities import dictParse
 path_binDEXCS = os.path.expanduser('~/Desktop/binDEXCS2019（解析フォルダを端末で開いてから）') # dakuten.py -j -f <path> で濁点を結合しておく
 sys.path.append(path_binDEXCS)
-
-def find_fields(dpl):
-    fields = []
-    for x in dpl:
-        if isinstance(x, (tuple, list)):
-            if DictParserList.isType(x, DictParserList.LISTP):
-                i = 0
-                y = x[2]
-                while i < len(y):
-                    if type(y[i]) is str and y[i] in ('volScalarFieldValue', 'volVectorFieldValue'):
-                        i += 2
-                        if i < len(y) and type(y[i]) is str:
-                            fields.append(y[i])
-                    elif isinstance(y[i], (tuple, list)):
-                        fields.extend(find_fields(y[i]))
-                    i += 1
-            else:
-                fields.extend(find_fields(x))
-    return list(set(fields))
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal.SIG_DFL) # Ctrl+Cで終了
@@ -59,59 +42,89 @@ if __name__ == '__main__':
                 set_for_orig = True
             i += 1
 
-    setFieldsDict = os.path.join('system', 'setFieldsDict')
+    setFieldsDict_path = os.path.join('system', 'setFieldsDict')
     if interactive:
         setFieldsDict_is_written = (True if (raw_input if sys.version_info.major <= 2 else input)(
-            setFieldsDict + 'ファイルの内容を確認して下さい．設定値に関する指示が書き込まれていますか？ (y/n) > '
-            ).strip().lower() == 'y' else False) if os.path.isfile(setFieldsDict) else False
+            setFieldsDict_path + 'ファイルの内容を確認して下さい．設定値に関する指示が書き込まれていますか？ (y/n) > '
+            ).strip().lower() == 'y' else False) if os.path.isfile(setFieldsDict_path) else False
 
     if not setFieldsDict_is_written:
-        if os.path.isfile(setFieldsDict):
-            os.rename(setFieldsDict, setFieldsDict + '_bak')
-        with open(setFieldsDict, 'w') as f:
-            f.write('FoamFile\n{\n\tversion\t2.0;\n\tformat\tascii;\n\tclass\tdictionary;\n')
-            f.write('\tlocation\t"system";\n')
-            f.write('\tobject\tsetFieldsDict;\n')
-            f.write('}\n')
-            f.write('\n// まず，領域全体で値を設定する．\n')
-            f.write('defaultFieldValues\n(\n')
-            f.write('\tvolScalarFieldValue /* 変数の名前 = */ alpha.water /* 値 = */ 0\n')
-            f.write(');\n')
-            f.write('\nregions\n(\n')
-            f.write('\t// boxで決められた直方体の範囲の値を設定する．\n')
-            f.write('\tboxToCell\n\t{\n')
-            f.write('\t\tbox\t/* (x_min y_min z_min) = */ (-1 -1 -1) /* (x_max y_max z_max) = */ (2 2 2);\n')
-            f.write('\t\tfieldValues\n\t\t(\n')
-            f.write('\t\t\tvolScalarFieldValue /* 変数の名前 = */ alpha.water /* 値 = */ 1\n')
-            f.write('\t\t);\n')
-            f.write('\t}\n')
-            f.write(');\n')
-        print('\n\033[3;4;5m' + setFieldsDict + 'ファイルにテンプレートを追加して，texteditwx.pyで開いています．')
+        if os.path.isfile(setFieldsDict_path):
+            os.rename(setFieldsDict_path, setFieldsDict_path + '_bak')
+        with open(setFieldsDict_path, 'w') as f:
+            f.write('FoamFile\n'
+                '{\n'
+                '\tversion\t2.0;\n'
+                '\tformat\tascii;\n'
+                '\tclass\tdictionary;\n'
+                '\tlocation\t"system";\n'
+                '\tobject\tsetFieldsDict;\n'
+                '}\n'
+                '\n'
+                'defaultFieldValues // まず，領域全体で値を設定する．\n'
+                '(\n'
+                '\tvolScalarFieldValue alpha.water 0 // スカラーの場合 -> 変数の名前 値\n'
+                '\tvolVectorFieldValue U (0 0 0) // ベクトルの場合 -> 変数の名前 (x成分 y成分 z成分)\n'
+                ');\n'
+                '\n'
+                'regions\n'
+                '(\n'
+                '\tboxToCell // boxで決められた直方体の範囲の値を設定する．\n'
+                '\t{\n'
+                '\t\tbox\t(-1 -1 -1) (2 2 2); // (x_min y_min z_min) (x_max y_max z_max)\n'
+                '\t\tfieldValues\n'
+                '\t\t(\n'
+                '\t\t\tvolScalarFieldValue alpha.water 1 // スカラーの場合 -> 変数の名前 値\n'
+                '\t\t\tvolVectorFieldValue U (1 1 1) // ベクトルの場合 -> 変数の名前 (x成分 y成分 z成分)\n'
+                '\t\t);\n'
+                '\t}\n'
+                ');\n')
+        print('\n\033[3;4;5m' + setFieldsDict_path + 'ファイルにテンプレートを追加して，texteditwx.pyで開いています．')
         print('説明コメントを読んで，自分が行いたいことに合わせてテンプレートを書き換えて下さい．')
         print('書き換えたらtexteditwx.pyを終了して下さい．\033[m\n')
-        subprocess.call(os.path.join(path_binDEXCS, 'texteditwx.py') + ' {}'.format(setFieldsDict), shell = True)
+        subprocess.call(os.path.join(path_binDEXCS, 'texteditwx.py') + ' ' + setFieldsDict_path, shell = True)
 
-    fields_to_be_set = find_fields(DictParser(setFieldsDict).contents)
-    orig_fields = []
-    for i in fields_to_be_set:
-        field = os.path.join('0', i)
-        if os.path.isfile(field + '.orig'):
-            orig_fields.append(i + '.orig')
+    setFieldsDict = dictParse.DictParser2(file_name = setFieldsDict_path)
+
+    fields_set = set()
+    fieldValues = (
+        setFieldsDict.find_all_elements(
+            [{'type': 'dictionary', 'key': 'defaultFieldValues'}, {'type': 'list'},
+            {'except type': 'ignorable|list_start|list_end'}]) +
+        setFieldsDict.find_all_elements(
+            [{'type': 'dictionary', 'key': 'regions'}, {'type': 'list'}, {'type': 'block'},
+            {'type': 'dictionary', 'value': 'fieldValues'}, {'type': 'list'},
+            {'except type': 'ignorable|list_start|list_end'}]))
+    i = 0
+    while i < len(fieldValues):
+        if (fieldValues[i]['element']['type'] == 'word' and
+            fieldValues[i]['element']['value'] in ('volScalarFieldValue', 'volVectorFieldValue')):
+            fields_set.add(fieldValues[i + 1]['element']['value'])
+            i += 3
+        else:
+            i += 1
+    fields_set = sorted(list(fields_set))
+
+    orig_fields = [i for i in fields_set if os.path.isfile(os.path.join('0', i) + '.orig')]
     if interactive and len(orig_fields) > 0:
-        set_for_orig = True if (raw_input if sys.version_info.major <= 2 else input)(
-            '0秒フォルダにある{}に対してsetFieldsを行いますか？ (y/n) > '.format(', '.join(orig_fields))
+        set_for_orig = True if input(
+            f'0秒フォルダにある{".orig, ".join(orig_fields)}.origに対してもsetFieldsを行いますか？ (y/n) > '
             ).strip().lower() == 'y' else False
     if set_for_orig:
         for i in orig_fields:
-            field = os.path.join('0', i)
-            shutil.copy(field, field[:-5])
-    for i in fields_to_be_set:
-        field = os.path.join('0', i)
-        shutil.copy(field, field + '.orig')
+            i = os.path.join('0', i)
+            shutil.copy(i + '.orig', i)
+        for i in set(fields_set) - set(orig_fields):
+            i = os.path.join('0', i)
+            shutil.copy(i, i + '.orig')
+    else:
+        for i in fields_set:
+            i = os.path.join('0', i)
+            shutil.copy(i, i + '.orig')
 
     command = 'setFields -noFunctionObjects'
     if subprocess.call(command, shell = True) != 0:
-        print('{}で失敗しました．よく分かる人に相談して下さい．'.format(command))
+        print(f'{command}で失敗しました．よく分かる人に相談して下さい．')
         sys.exit(1)
 
     print('setFieldsで書き換えられる前のファイルは，末尾に.origをつけた名前でバックアップされています．')
