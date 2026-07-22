bed082cf5d858be5fa99ad5188373fba279d5c1f
gitwamoto <98292412+gitwamoto@users.noreply.github.com>
2026-07-22 22:10:13 +0900

    Update 計算.py

--- makeConvenient/resources/binDEXCS（解析フォルダを端末で開いてから）/計算.py
+++ makeConvenient/resources/binDEXCS（解析フォルダを端末で開いてから）/計算.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # 計算.py
 # by Yukiharu Iwamoto
-# 2026/7/22 3:23:30 PM
+# 2026/7/22 10:09:59 PM
 
 # ---- オプション ----
 # なし -> インタラクティブモードで実行．オプションが1つでもあると非インタラクティブモードになる
@@ -40,8 +40,8 @@ from utilities import dictParse
 # plt.rcParams['figure.figsize'] = (6.0, 3.6) # (width, height), デフォルト値は環境によりますが、多くの場合は (6.4, 4.8) です。
 
 relaxationFactor_lower_limit = 0.3  # 緩和係数の下限値
-relaxationFactor_delta_usual = 0.01  # 通常時における緩和係数の変化量の絶対値
-relaxationFactor_delta_restart = 0.05  # リスタート時における緩和係数の変化量の絶対値
+relaxationFactor_delta_usual = 0.002  # 通常時における緩和係数の変化量の絶対値
+relaxationFactor_delta_restart = 0.05  # リスタート時における緩和係数の減量量
 domains = 1
 regionProperties_path = os.path.join("constant", "regionProperties")
 decomposeParDict_path = os.path.join("system", "decomposeParDict")
